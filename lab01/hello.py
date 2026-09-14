76bcd06eee56f88ed8614b9c122c067c4edffe4c
EmmaHyland8 <C00321934@setu.ie>
2026-09-14 11:46:01 +0000

    saving work

--- lab01/hello.py
+++ lab01/hello.py
@@ -1,1 +1,7 @@
-print("Hello, world!")
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+studying = input("what are you studying?")
+favourite = input("whats your favourite part of " + studying)
+print("your favourite part is" + favourite )
+
+
